1a5670f34fcf4182d8c4b93d303fe6292b9e491c
awais <awais.aamir@outlook.com>
2024-09-10 18:35:19 +0100

    Updated price plans and can now toggle them between monthly and annually

--- js/script.js
+++ js/script.js
@@ -20,3 +20,40 @@ mealCards.forEach((card) => {
   });
 });
 
+//---- Pricing Plan
+
+const pricingBtn = document.querySelector(".pricing-btn");
+const monthlyBtn = document.querySelector(".monthly-btn")
+const annualBtn = document.querySelector(".annual-btn")
+const basicPrice = document.querySelector(".plan-price-basic");
+const compPrice = document.querySelector(".plan-price-comp");
+const planTxt = document.querySelector(".plan-txt");
+
+annualBtn.addEventListener("click", function(e) {
+  e.preventDefault(); // Prevent the default behavior (if necessary)
+  
+  // Change the background gradient of pricingBtn
+  pricingBtn.style.background = 'linear-gradient(to left, #b3d9d9 47%, #ffffff 50%)';
+  monthlyBtn.classList.remove("active-btn");
+  monthlyBtn.classList.add("inactive-btn")
+  annualBtn.classList.remove("inactive-btn")
+  annualBtn.classList.add("active-btn");
+
+  compPrice.textContent = (`£7788`);
+  basicPrice.textContent = (`£4788`);
+  
+});
+
+monthlyBtn.addEventListener("click", function(e) {
+  e.preventDefault(); // Prevent the default behavior (if necessary)
+  
+  // Change the background gradient of pricingBtn
+  pricingBtn.style.background = 'linear-gradient(to right, #b3d9d9 53%, #ffffff 50%)';
+  monthlyBtn.classList.add("active-btn");
+  monthlyBtn.classList.remove("inactive-btn")
+  annualBtn.classList.add("inactive-btn")
+  annualBtn.classList.remove("active-btn");
+
+  compPrice.textContent = ('£649');
+  basicPrice.textContent = ('£399');
+});
